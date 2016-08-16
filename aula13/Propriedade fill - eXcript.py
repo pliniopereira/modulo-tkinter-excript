7dbdafc6bc1521a9cca7c318958aa3ffd8736fd5
Plínio Pereira <pliniojr@gmail.com>
2016-08-16 15:41:28 -0300

    Módulo Tkinter - Aula 13 - Propriedade fill - eXcript :white_check_mark:

---
+++ aula13/Propriedade fill - eXcript.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+janela = Tk()
+
+lb1 = Label(janela, text="horizontal", bg="white")
+lb2 = Label(janela, text="", bg="black")
+lb3 = Label(janela, text="", bg="black")
+
+lb1.pack(side=TOP, fill=X)
+lb2.pack(side=LEFT, fill=Y)
+lb3.pack(side=RIGHT, fill=Y)
+
+janela.geometry("400x300+200+200")
+janela.mainloop()
