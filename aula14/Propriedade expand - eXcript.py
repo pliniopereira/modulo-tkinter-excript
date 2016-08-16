23af78c249ad7079b358d2d1ed2d93d641b7d0fc
Plínio Pereira <pliniojr@gmail.com>
2016-08-16 15:46:01 -0300

    Módulo Tkinter - Aula 14 - Propriedade expand - eXcript :white_check_mark:

---
+++ aula14/Propriedade expand - eXcript.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+janela = Tk()
+
+lb1 = Label(janela, text="Linha 1", bg="blue")
+lb2 = Label(janela, text="Linha 2", bg="yellow")
+lb3 = Label(janela, text="Linha 3", bg="blue")
+lb4 = Label(janela, text="Linha 4", bg="yellow")
+
+
+lb1.pack(side=TOP, fill=BOTH, expand=1)
+lb2.pack(side=TOP, fill=BOTH, expand=1)
+lb3.pack(side=TOP, fill=BOTH, expand=1)
+lb4.pack(side=TOP, fill=BOTH, expand=1)
+
+
+janela.geometry("400x200+-600+200")
+
+janela.mainloop()
