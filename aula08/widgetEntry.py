49e853e8726765588505bcaaafa25dc825555cbd
Plínio Pereira <pliniojr@gmail.com>
2016-08-16 13:40:32 -0300

    Extraindo valores do Widget Entry - eXcript  :white_check_mark:

---
+++ aula08/widgetEntry.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+def bt_onclick():
+    print(ed.get())
+    lb["text"] = ed.get()
+
+
+janela = Tk()
+
+ed = Entry(janela)
+ed.place(x=100, y=100)
+
+bt = Button(janela, width=20, text="OK")
+bt.place(x=100, y=150)
+
+bt = Button(janela, width=20, text="OK", command=bt_onclick)
+bt.place(x=100, y=150)
+
+lb = Label(janela, text="Label")
+lb.place(x=100, y=200)
+
+janela.geometry("300x300+200+200")
+janela.mainloop()
